68293b9fa6ff4bd1b1911ee6835d43c34add5548
Jarek <jarek@Zipcoders-MacBook-Pro-65.local>
2023-03-31 19:24:08 -0400

    Forward and backward counters work

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,37 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+        int forward = forwardCount(startIndex, selection);
+        //backward
+        int back = backwardCount(startIndex, selection);
+        if(forward < back){
+            return forward;
+        }else{
+            return back;
+        }
     }
+    public Integer forwardCount(Integer startIndex, String selection){
+        int forwardCounter = 0;
+        for (int i = startIndex; !playList[i].contains(selection); i++) {
+            if(i == playList.length-1){
+                i = -1;
+            }
+            if(!playList[i].matches(selection)){
+            forwardCounter++;
+            }
+        }
+        return forwardCounter;
+    }
+    public Integer backwardCount(Integer startIndex, String selection){
+        int backCounter = 0;
+        for (int i = startIndex; !playList[i].contains(selection); i--) {
+            if(i == 0){
+                i = playList.length;
+            }
+                backCounter++;
+        }
+        return backCounter;
+    }
+
 }
